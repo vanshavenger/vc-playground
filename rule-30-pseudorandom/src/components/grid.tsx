99e4bbafd434c853f4adbdd467e991e60030e5ac
vanshavenger <vanshchopra101@gmail.com>
2024-12-26 23:09:15 +0530

    Grid function

--- rule-30-pseudorandom/src/components/grid.tsx
+++ rule-30-pseudorandom/src/components/grid.tsx
@@ -12,5 +12,25 @@ export const Grid = ({ grid }: GridProps) => {
 			scrollRef.current.scrollTop = scrollRef.current.scrollHeight
 		}
 	}, [grid])
-	return <div></div>
+
+	return (
+		<div
+			ref={scrollRef}
+			className='h-96 overflow-auto border border-gray-200 bg-gray-100'
+		>
+			{grid.map((row, i) => (
+				<div key={i} className='flex'>
+					{row.map((cell, j) => (
+						<div
+							key={`${i}-${j}`}
+							className={`w-4 h-4 border border-gray-300 ${
+								cell ? 'bg-black' : 'bg-white'
+							}`}
+							style={{ aspectRatio: '1/1' }}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	)
 }
