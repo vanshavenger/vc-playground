2f778dd09c590d5f48bd6d715de166c0c3283ca1
vanshavenger <vanshchopra101@gmail.com>
2025-01-11 22:09:47 +0530

    Reafctor

--- excelidraw/packages/ui/src/card.tsx
+++ excelidraw/packages/ui/src/card.tsx
@@ -1,4 +1,4 @@
-import { type JSX } from "react";
+import { type JSX } from 'react'
 
 export function Card({
   className,
@@ -6,22 +6,22 @@ export function Card({
   children,
   href,
 }: {
-  className?: string;
-  title: string;
-  children: React.ReactNode;
-  href: string;
+  className?: string
+  title: string
+  children: React.ReactNode
+  href: string
 }): JSX.Element {
   return (
     <a
       className={className}
       href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
+      rel='noopener noreferrer'
+      target='_blank'
     >
       <h2>
         {title} <span>-&gt;</span>
       </h2>
       <p>{children}</p>
     </a>
-  );
+  )
 }
