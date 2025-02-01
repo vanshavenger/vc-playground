d8f44cdc9f8693306db2840d5eea33eb41dc6342
vanshavenger <vanshchopra101@gmail.com>
2025-02-01 20:24:51 +0530

    Invoice schema

--- agno/pdf-rag/main.py
+++ agno/pdf-rag/main.py
@@ -1,7 +1,8 @@
 import os
 
 from agno.models.azure import AzureOpenAI
-
+from pydantic import BaseModel, Field
+from typing import Optional
 from agno.embedder.azure_openai import AzureOpenAIEmbedder
 from dotenv import load_dotenv
 
@@ -10,6 +11,23 @@ load_dotenv()
 AZURE_MODEL_DEPLOYMENT = "gpt4o"
 AZURE_EMBEDDER_DEPLOYMENT = "text-embedding-ada-002"
 AZURE_EMBEDDER_MODEL = "text-embedding-ada-002"
+
+class InvoiceItem(BaseModel):
+    item: str = Field(..., description="Name of the item")
+    description: str = Field(..., description="Description of the item")
+    quantity: float = Field(..., description="Quantity of the item")
+    unit_price: float = Field(..., description="Unit price of the item")
+    tax: Optional[float] = Field(None, description="Tax amount for the item")
+    subtotal: float = Field(..., description="Subtotal for the item")
+
+
+class InvoiceData(BaseModel):
+    invoice_number: str = Field(..., description="Invoice number")
+    items: list[InvoiceItem] = Field(..., description="List of items in the invoice")
+    total_amount: float = Field(..., description="Total amount of the invoice")
+    total_tax: Optional[float] = Field(
+        None, description="Total tax amount for the invoice"
+    )
 
 def setup_azure_model():
     return AzureOpenAI(
