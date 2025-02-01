2b78d752835fa36eb3dd86d5105b175ec8a5cad1
vanshavenger <vanshchopra101@gmail.com>
2025-02-01 20:26:23 +0530

    Pdf knowledge base

--- agno/pdf-rag/main.py
+++ agno/pdf-rag/main.py
@@ -5,12 +5,17 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from agno.embedder.azure_openai import AzureOpenAIEmbedder
 from dotenv import load_dotenv
+from pathlib import Path
+from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
+from agno.vectordb.pgvector import PgVector, SearchType
 
 load_dotenv()
 
 AZURE_MODEL_DEPLOYMENT = "gpt4o"
 AZURE_EMBEDDER_DEPLOYMENT = "text-embedding-ada-002"
 AZURE_EMBEDDER_MODEL = "text-embedding-ada-002"
+DB_URL = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+PDF_TABLE_NAME = "pdf_documents"
 
 class InvoiceItem(BaseModel):
     item: str = Field(..., description="Name of the item")
@@ -43,6 +48,20 @@ def setup_azure_embedder():
         api_key=os.getenv("AZURE_OPENAI_API_KEY"),
         azure_endpoint=os.getenv("AZURE_API_BASE"),
         azure_deployment=AZURE_EMBEDDER_DEPLOYMENT,
+    )
+    
+def setup_pdf_knowledge_base(pdf_path: str | Path, azure_embedder: AzureOpenAIEmbedder):
+    return PDFKnowledgeBase(
+        path=pdf_path,
+        vector_db=PgVector(
+            table_name=PDF_TABLE_NAME,
+            db_url=DB_URL,
+            search_type=SearchType.hybrid,
+            embedder=azure_embedder,
+        ),
+        reader=PDFReader(chunk=True),
+        num_documents=1,
+        optimize_on=1,
     )
 
 def process_invoices():
