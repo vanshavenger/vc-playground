c2d05b4c0b0e03baafa48eac4cacd9ec9d1a8827
vanshavenger <vanshchopra101@gmail.com>
2025-04-06 16:20:45 +0530

    agno tools

--- mcp/test-agno-tools/hello.py
+++ mcp/test-agno-tools/hello.py
@@ -5,11 +5,32 @@ from agno.tools.thinking import ThinkingTools
 from agno.knowledge.url import UrlKnowledge
 from agno.vectordb.pgvector import PgVector, SearchType
 from agno.embedder.ollama import OllamaEmbedder
+from agno.models.google import Gemini
+from agno.tools.sleep import SleepTools
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+
+def sleep(seconds=10):
+    """Sleep for a given number of seconds.
+
+    Args:
+        seconds (int): Number of seconds to sleep. Default is 10.
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
+    import time
+    time.sleep(seconds)
+
 
 knowledge_base = UrlKnowledge(
     urls=["https://sdk.vercel.ai/llms.txt"],
@@ -23,19 +44,27 @@ knowledge_base = UrlKnowledge(
 # knowledge_base.load(recreate=True, upsert=True)
 
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Gemini(id="gemini-2.5-pro-preview-03-25"),
     markdown=True,
-    tools=[ThinkingTools(add_instructions=True)],
+    tools=[ThinkingTools(add_instructions=True), SleepTools()],
     show_tool_calls=True,
     knowledge=knowledge_base,
     add_references=True,
     debug_mode=True,
     search_knowledge=True,
+    instructions="""
+    You are a helpful assistant.
+    You have access to a knowledge base.
     
+    ### Sleep Tool
+    Use it after each step and sleep for 10 seconds
+    
+    """
+
 )
 
 agent.print_response(
-    "Give me the way to use tools with stream object", stream=True)
+    "Best model on vercel ai sdk",)
 
 
 for key in list(os.environ.keys()):
